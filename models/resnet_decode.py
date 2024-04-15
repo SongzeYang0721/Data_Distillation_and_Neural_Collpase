65a793b312811e12dca58e4c9b70fa7e6f07dc51
Songze_Yang <yangsongze2008@gmail.com>
2024-04-15 13:19:19 +1000

    no norm layer

--- models/resnet_decode.py
+++ models/resnet_decode.py
@@ -49,56 +49,6 @@ def deconv1x1(in_planes: int, out_planes: int, stride: int = 1, output_padding: 
         output_padding=output_padding
         )
 
-
-# class BasicBlock(nn.Module):
-#     """The basic block architecture of resnet-18 network.
-#     """
-#     expansion: int = 1
-
-#     def __init__(
-#         self,
-#         inplanes: int,
-#         planes: int,
-#         stride: int = 1,
-#         output_padding: int = 0,
-#         upsample: Optional[nn.Module] = None,
-#         groups: int = 1,
-#         base_width: int = 64,
-#         dilation: int = 1,
-#         norm_layer: Optional[Callable[..., nn.Module]] = None,
-#     ) -> None:
-#         super().__init__()
-#         if norm_layer is None:
-#             norm_layer = nn.BatchNorm2d
-#         if groups != 1 or base_width != 64:
-#             raise ValueError("BasicBlock only supports groups=1 and base_width=64")
-#         if dilation > 1:
-#             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-#         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-#         self.conv1 = deconv3x3(planes, inplanes, stride, output_padding=output_padding)
-#         self.bn1 = norm_layer(inplanes)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.conv2 = deconv3x3(planes, planes)
-#         self.bn2 = norm_layer(planes)
-#         self.upsample = upsample
-#         self.stride = stride
-
-#     def forward(self, x: Tensor) -> Tensor:
-#         identity = x
-#         out = self.conv2(x)
-#         out = self.bn2(out)
-#         out = self.relu(out)
-
-#         out = self.conv1(out)
-#         out = self.bn1(out)
-
-#         if self.upsample is not None:
-#             identity = self.upsample(x)
-
-#         out += identity
-#         out = self.relu(out)
-#         return out
-
 class BasicBlock(nn.Module):
     expansion: int = 1
 
@@ -237,8 +187,8 @@ class ResNet(nn.Module):
         SOTA: bool = False,
     ) -> None:
         super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
+        # if norm_layer is None:
+        #     norm_layer = nn.BatchNorm2d
         self._norm_layer = norm_layer
 
         self.inplanes = 64
