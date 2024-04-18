3f5ac98a79f39ee93f19cadf1f2bb225021a73a7
Songze_Yang <yangsongze2008@gmail.com>
2024-04-18 12:02:14 +1000

    on test ETF

--- validate_NC.py
+++ validate_NC.py
@@ -325,21 +325,20 @@ def evaluate_NC(args,load_path,model,trainloader,testloader,nearest_neighbor = F
                             "nearest neighbor (Test ETF): test_acc5":near_test_acc5_ontest
                             })
             else:
-                if 'wandb' in sys.modules and ontest:
-            wandb.log({
-                        "train_acc1":train_acc1, 
-                        "train_acc5":train_acc5,
-                        "test_acc1":test_acc1,
-                        "test_acc5":test_acc5,
-                        "collapse_metric":collapse_metric, 
-                        "ETF_metric":ETF_metric, 
-                        "WH_relation_metric":WH_relation_metric,
-                        "Wh_b_relation_metric":Wh_b_relation_metric,
-                        "nearest neighbor: train_acc1":near_train_acc1, 
-                        "nearest neighbor: train_acc5":near_train_acc5,
-                        "nearest neighbor: test_acc1":near_test_acc1,
-                        "nearest neighbor: test_acc5":near_test_acc5
-                        })
+                wandb.log({
+                            "train_acc1":train_acc1, 
+                            "train_acc5":train_acc5,
+                            "test_acc1":test_acc1,
+                            "test_acc5":test_acc5,
+                            "collapse_metric":collapse_metric, 
+                            "ETF_metric":ETF_metric, 
+                            "WH_relation_metric":WH_relation_metric,
+                            "Wh_b_relation_metric":Wh_b_relation_metric,
+                            "nearest neighbor: train_acc1":near_train_acc1, 
+                            "nearest neighbor: train_acc5":near_train_acc5,
+                            "nearest neighbor: test_acc1":near_test_acc1,
+                            "nearest neighbor: test_acc5":near_test_acc5
+                            })
             
     with open(args.load_path + 'info.pkl', 'wb') as f:
         pickle.dump(info_dict, f)
