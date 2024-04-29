0af2f1132b07acfdb11d00c1a491580a860eef62
Songze_Yang <yangsongze2008@gmail.com>
2024-04-29 11:06:36 +1000

    success in the 256 dimension, further evaluation

--- train_Encoder.py
+++ train_Encoder.py
@@ -160,7 +160,7 @@ def train(args, model, trainloader):
             trainer_2nd(args, model, trainloader, epoch_id, criterion, optimizer)
         else:
             trainer_1st(args, model, trainloader, epoch_id, criterion, optimizer, scheduler)
-        # torch.save(model.state_dict(), args.save_path + "/epoch_" + str(epoch_id + 1).zfill(3) + ".pth")
+        torch.save(model.state_dict(), args.save_path + "/epoch_" + str(epoch_id + 1).zfill(3) + ".pth")
         columns=["id", "image", "guess", "truth"]
         for digit in range(10):
             columns.append("score_" + str(digit))
