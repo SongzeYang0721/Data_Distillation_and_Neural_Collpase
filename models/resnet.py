1039e27fc3f171aef74a79df5f5f5d7f4351972c
Songze_Yang <yangsongze2008@gmail.com>
2024-05-02 14:55:16 +1000

    Try to exclude the avgpoll and the normlaize layers for the encoder. Expectation: the encoder should have the neural collapse and the decoder should have a performance boost

--- models/resnet.py
+++ models/resnet.py
@@ -299,9 +299,12 @@ class ResNet(nn.Module):
 
         # return x
         
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
-        features = F.normalize(x) # normalized H ready to feed to the linear layer
+        # x = self.avgpool(x)
+        # x = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
+        # features = F.normalize(x) # normalized H ready to feed to the linear layer
+        # x = self.fc(x)
+
+        features = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
         x = self.fc(x)
 
         return x, features
