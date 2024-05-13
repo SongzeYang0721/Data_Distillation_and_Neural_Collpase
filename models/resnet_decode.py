40a40d20aa72cefe9145afedc2a3ad718affeec6
Songze_Yang <yangsongze2008@gmail.com>
2024-05-13 20:45:18 +1000

    try a smaller model

--- models/resnet_decode.py
+++ models/resnet_decode.py
@@ -348,7 +348,7 @@ class ResNet(nn.Module):
     def _forward_impl(self, x: Tensor):
 
         x = x.view(x.shape[0], x.shape[1], 1, 1)
-        x = self.unnormalize(x)
+        # x = self.unnormalize(x)
         x = self.unavgpool(x)
 
         x = self.layer4(x)
