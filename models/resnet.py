f682f8e1d59059fd96e1c1c6805dea7e365d56d2
Songze_Yang <yangsongze2008@gmail.com>
2024-05-13 17:51:18 +1000

    try a smaller model

--- models/resnet.py
+++ models/resnet.py
@@ -235,7 +235,7 @@ class ResNet(nn.Module):
                     if fixdim:
                         m.weight = nn.Parameter(weight)
                     else:
-                        m.weight = nn.Parameter(torch.mm(weight, torch.eye(num_classes, 2048 * block.expansion)))
+                        m.weight = nn.Parameter(torch.mm(weight, torch.eye(num_classes, 512 * block.expansion)))
                         # m.weight = nn.Parameter(torch.mm(weight, torch.eye(num_classes, 256 * block.expansion)))
                     m.weight.requires_grad_(False)
 
@@ -295,20 +295,20 @@ class ResNet(nn.Module):
 
         x = self.layer1(x)
         x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
         # return x
         
-        # x = self.avgpool(x)
-        # x = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
-        # features = F.normalize(x) # normalized H ready to feed to the linear layer
-        # x = self.fc(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
+        features = F.normalize(x) # normalized H ready to feed to the linear layer
+        x = self.fc(x)
 
         # this a all conv version of resnet18
-        x = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
-        features = x
-        x = self.fc(x)
+        # x = torch.flatten(x, 1) # flatten the second dimension from (n, m, k) to (n, m*k), here m*k = d
+        # features = x
+        # x = self.fc(x)
 
         return x, features
 
--- models/resnet_decode.py
+++ models/resnet_decode.py
@@ -348,11 +348,11 @@ class ResNet(nn.Module):
     def _forward_impl(self, x: Tensor):
 
         x = x.view(x.shape[0], x.shape[1], 1, 1)
-        # x = self.unnormalize(x)
-        # x = self.unavgpool(x)
+        x = self.unnormalize(x)
+        x = self.unavgpool(x)
 
-        # x = self.layer4(x)
-        # x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer3(x)
         x = self.layer2(x)
         x = self.layer1(x)
 
