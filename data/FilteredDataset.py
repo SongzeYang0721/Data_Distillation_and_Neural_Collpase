9699c69faba2060666fabf6e2029abf5c41ca5bb
Songze_Yang <yangsongze2008@gmail.com>
2024-05-24 14:07:06 +1000

    fix the bug

--- data/FilteredDataset.py
+++ data/FilteredDataset.py
@@ -1,23 +1,20 @@
 import torch
 from torch.utils.data import Dataset
-
 class FilteredDataset(Dataset):
     def __init__(self, dataset, classes_to_include):
         self.dataset = dataset
         self.classes_to_include = classes_to_include
-        self.indices = self._get_filtered_indices()
+        self.label_map = {original_label: new_label for new_label, original_label in enumerate(classes_to_include)}
+        self.filtered_indices = self._get_filtered_indices()
 
     def _get_filtered_indices(self):
-        filtered_indices = []
-        for idx, (_, label) in enumerate(self.dataset):
-            if label in self.classes_to_include:
-                filtered_indices.append(idx)
-        return filtered_indices
+        return [idx for idx, (_, label) in enumerate(self.dataset) if label in self.classes_to_include]
 
     def __len__(self):
-        return len(self.indices)
+        return len(self.filtered_indices)
 
     def __getitem__(self, idx):
-        actual_idx = self.indices[idx]
+        actual_idx = self.filtered_indices[idx]
         image, label = self.dataset[actual_idx]
-        return image, label
+        remapped_label = self.label_map[label]
+        return image, remapped_label
